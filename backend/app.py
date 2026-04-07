a1ccdc50e73d7d1a604e0001f73c8433a333c7b1
Danish3803 <danishsheikh3803@gmail.com>
2026-04-07 14:14:24 +0530

    render backend fixed

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from flask import Flask, request, jsonify, redirect
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["https://cyber-sentinel-evxklgflx-danish-sheikhs-projects.vercel.app"])  
+CORS(app, origins=["https://cyber-sentinel-ai-alpha.vercel.app"])  
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(BASE_DIR))
